051722ca743faaf4b12c97729ebf20f968e8cd6c
EC2 Default User <ec2-user@ip-172-31-7-190.eu-west-1.compute.internal>
2018-04-13 15:00:14 +0000

    updated queries

--- redisproject/queries.js
+++ redisproject/queries.js
@@ -5,7 +5,13 @@ client.on("error", function(err) {
     console.log("Error " + err);
 });
 
-var id = 0;
+
+
+var id;
+client.get('lastid', function(error, value) {
+    id = value;
+});
+
 
 // add query functions
 
@@ -49,34 +55,26 @@ function getSingleCurrency(req, res, next) {
 }
 
 function createCurrency(req, res, next) {
-    client.set(id, JSON.stringify(req.body), function(error) {
-        console.log("id: " + id + "request: " + JSON.stringify(req.body) + "error: " + error);
-        res.status(200)
-            .json({
-                status: 'success',
-                message: 'Inserted one currency'
-            });
-    })
-    client.sadd('listofkeys', id);
     id++;
+    client.multi()
+        .set('lastid', id)
+        .set(id, JSON.stringify(req.body))
+        .sadd('listofkeys', id)
+        .exec(function(error, replies) {
+            console.log("id: " + id + "request: " + JSON.stringify(req.body) + "error: " + error);
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one currency'
+                });
 
-
+        });
 }
 /*
 example input to curl
  curl -X POST -H "Content-Type: application/json" --data '{"name":"andy","symbol":"A","amount":"1500"}' \http://127.0.0.1:8080/api/cryptocurrency/2
 */
 function updateCurrency(req, res, next) {
-    // console.log(req.body)
-    // var args = [Object.keys(req.body).length*2];
-    // for (var i = 0; i < Object.keys(req.body).length; i++) {
-    //     args[i*2] = Object.keys(req.body)[i];
-
-    //     var key = args[i];
-    //     console.log(req.body[key] + " " + i);
-    //   args[(i*2)+1] = req.body[key];
-    // }
-    // console.log(args);
 
     client.set(req.params.id, JSON.stringify(req.body), function(error, value) {
         console.log("request: " + JSON.stringify(req.body) + " error: " + error + " value: " + value + "id " + req.params.id)
@@ -92,14 +90,15 @@ function updateCurrency(req, res, next) {
 }
 
 function removeCurrency(req, res, next) {
-
-    client.del(req.params.id, function(error, value) {
-        console.log("id: " + req.params.id + "error: " + error);
-        res.status(200)
-            .json({
-                status: 'success',
-                message: 'Removed ${result.rowCount} currency'
-            });
-    })
-    id--;
+    client.multi()
+        .del(req.params.id)
+        .srem('listofkeys', req.params.id)
+        .exec(function(error, replies) {
+            console.log("id: " + req.params.id + "error: " + error);
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Removed currency'
+                });
+        })
 }
